14e809d526d4c57acaa695ee7738cd07aec26cdd
Gerardo Alberto Soto Alvarez del Castillo <gerardo.8.soto@gmail.com>
2021-08-21 00:53:34 -0500

    Declaraciones de funciones con parametros entre EMACScript 5- y 6+, Uso de Templates Literals entre EMACScript 5- y 6+.

--- src/es6/index.js
+++ src/es6/index.js
@@ -1,3 +1,33 @@
-funcion newFuncion() {
+// emac5- : Declaración de funciones con parametros
 
-}
+function newFuncion(username, age, country) {
+    var username = username || 'Default';
+    var age = age || 0;
+    var country = country || 'MX';
+    console.log(username, age, country);
+};
+
+// emacScript6+
+
+function newFunction6(name = 'Default', age = 0, country = 'MX') {
+    console.log(name, age, country);
+};
+
+// llamar a la función:
+newFunction6();
+newFunction6('Gerardo', 27, 'US');
+
+
+// Template Literals: Separar o unir varios elementos
+
+let hello = "Hello";
+let world = "World";
+
+// EMACScript 5-
+let epicphrase = hello + ' ' + world;
+console.log(epicphrase);
+
+// EMACScript 6+
+let epicphrase6 = `${hello} ${world}`;
+console.log(epicphrase6);
+
