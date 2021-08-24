b3e6068afc79fc03a80a349a88a7325b7dbb6e24
Gerardo Alberto Soto Alvarez del Castillo <gerardo.8.soto@gmail.com>
2021-08-23 22:18:08 -0500

    Caracteristicas agregadas en es7 y es8.

---
+++ src/es7/index.js
@@ -0,0 +1,18 @@
+// Buscar un valor en un array
+
+let numbers = [1,2,3,4,6,8];
+
+if (numbers.includes(7)) {
+    console.log('Si se encuentra el valor 7');
+}else{
+    console.log('No se encuentra el valor 7');
+}
+
+
+// el uso de potencias
+
+let base = 5;
+let exponente = 8;
+let resultado = base ** exponente;
+
+console.log(resultado);
---
+++ src/es8/index.js
@@ -0,0 +1,58 @@
+// Object.entries
+// Devolver la clave y los valores de una matriz.
+
+const data = {
+    frontend: 'oscar',
+    backend: 'gerard',
+    desing: 'ana',
+}
+
+const entries = Object.entries(data);
+console.log(entries);
+
+// ¿Cuántos elementos tiene un objeto? con la ayuda de entries:
+
+console.log(entries.length);
+
+
+
+const data2 = {
+    frontend: 'oscar',
+    backend: 'gerard',
+    desing: 'ana',
+}
+
+const values = Object.values(data);
+console.log(values);
+
+
+// Async y Await
+
+const helloWorld = () => {
+    return new Promise((resolve, reject) => {
+        (true) 
+        ? setTimeout(() => resolve('Hello, World!'), 3000)
+        : reject(new Error('Test error 1'))
+    })
+};
+
+
+const helloAsync = async () => {
+    const hello = await helloWorld();
+    const bye = await helloWorld();
+    console.log(hello);
+    console.log(bye);
+};
+
+helloAsync();
+
+// Forma correcta de utilizar ASYNC y promesas con try/catch
+
+const anotherHelloAsync = async () => {
+    try {
+        const hello = helloWorld();
+        console.log(hello);
+    } catch (error){
+        console.log(error);
+    }
+};
